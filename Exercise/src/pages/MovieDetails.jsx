841b336024e8d2e97c8dd2a57e7a7b86897ffe70
MinaFouad <mina.fouad@edgecome.com>
2025-07-29 03:06:00 +0200

    a

--- Exercise/src/pages/MovieDetails.jsx
+++ Exercise/src/pages/MovieDetails.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import {dummyDateTimeData, dummyShowsData} from '../assets/assets'
+import { LoaderIcon } from 'lucide-react'
 
 const MovieDetails = () => {
-  const id=useParams()
-  return (
-    <div>MovieDetails</div>
-  )
+  const {id}=useParams()
+  console.log(id)
+  const [show,setShow]=useState(null)
+  const getShow = async ()=>{
+    console.log(dummyShowsData)
+    const data =dummyShowsData.find(m=>m._id===id)
+    setShow({
+      movie:data,
+      dateTime:dummyDateTimeData
+    })
+  }
+  useEffect(()=>{
+       getShow()
+  },[id])
+
+  return show ? (
+    <div className='flex justify-between items-center '>
+        <img className='rounded-full ' 
+        src={show.movie.poster_path} alt={show.movie.title}/>  
+         
+    </div>
+
+
+
+
+  ):
+   (
+    <div>
+       <LoaderIcon></LoaderIcon>
+    </div>
+   )
 }
 
 export default MovieDetails
